82b9115fcc12e84d705245792a549924e0381762
Elijah Thomas <elijahthomas774@gmail.com>
2023-07-10 15:49:30 -0400

    commented out source files for dBase and fBase

---
+++ src/d/d_base.cpp
@@ -0,0 +1,26 @@
+#include <d/d_base.h>
+
+// dBase_c::dBase_c() {}
+
+// void dBase_c::postDraw(fBase_c::MAIN_STATE_e status) {}
+
+// int dBase_c::preExecute() {}
+
+// void dBase_c::postExecute(fBase_c::MAIN_STATE_e status) {}
+
+// int dBase_c::preDraw() {}
+
+// void dBase_c::resetFlags() {}
+
+// bool dBase_c::isActorPlayer(dBase_c& base) {}
+
+// int dBase_c::loadAsyncCallback() {}
+
+// void dBase_c::unloadCallback() {}
+
+// void dBase_c::initLoader() {}
+
+// dBase_c* dBase_c::createBase(ProfileName profName, dBase_c* parent, unsigned long param, u8 grouptype) {}
+
+// dBase_c* dBase_c::createRoot(ProfileName profName, unsigned long param, u8 groupType) {}
+
